9509e01414ce7d51d9c9165de1895a97fb2d9d63
Alan Rojas <109986279+echoalan@users.noreply.github.com>
2023-11-07 12:03:35 -0300

    Update simulation.js

--- simulation.js
+++ simulation.js
@@ -1,9 +1,9 @@
 function generarNumeroAleatorio(media, desviacionEstandar) {
     let numeroAleatorio;
     while (true) {                                            //[-1, 1]
-        numeroAleatorio = media + desviacionEstandar * (2 * Math.random() - 1);
+        numeroAleatorio = media + desviacionEstandar * (2 * Math.random() - 1); 
         if (numeroAleatorio >= 0) {
-            return numeroAleatorio;
+            return numeroAleatorio;   
         }
     }
 }
@@ -11,20 +11,20 @@ function generarNumeroAleatorio(media, desviacionEstandar) {
 
 function simularReservas(numSaturdays) {
     const totalMesas = 30; 
-    const media = 30; // Media de la distribución de probabilidad.
-    const desviacionEstandar = 3; // Desviación estándar de la distribución de probabilidad.
+    const media = 30; 
+    const desviacionEstandar = 3; 
     const numerosAleatorios = [];
 
     for (let i = 1; i <= numSaturdays; i++) {
-        // Generamos un número aleatorio que simula la cantidad de reservas.
-        const numeroReservas = Math.round(generarNumeroAleatorio(media, desviacionEstandar));
         
-        // Calculamos el porcentaje de reservas para este sábado.
-        const porcentajeReservas = (numeroReservas / totalMesas) * 100;
+        const numeroReservas = Math.round(generarNumeroAleatorio(media, desviacionEstandar)); // número aleatorio que simula la cantidad de reservas.
         
-        numerosAleatorios.push(numeroReservas);
+       
+        const porcentajeReservas = (numeroReservas / totalMesas) * 100;  // porcentaje de reservas para este sábado.
+         
+        numerosAleatorios.push(numeroReservas); //recordatorio de alan para alan: estaria bueno ver el numero random de ese sabado y no todos juntos
         
-        console.log(`Sábado ${i}: Reservas = ${numeroReservas}, Porcentaje = ${porcentajeReservas.toFixed(2)}%`);
+        console.log(`Sábado ${i}: Reservas = ${numeroReservas}, Porcentaje = ${porcentajeReservas.toFixed(2)}%`); //alan del futuro, si queres visualizar las cosas en el dom, dejá de atar con alambre!!!
     }
 
    
@@ -32,3 +32,5 @@ function simularReservas(numSaturdays) {
 }
 
 simularReservas(50); //50 sabados
+
+//alan del futuro, estaria bueno codear en ingles!!!!!!!!!!! cabeza de termo 
